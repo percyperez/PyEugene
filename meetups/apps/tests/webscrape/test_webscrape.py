9e71ee997ec9b3d5e5aa5a674e9a57191fe7bc80
Percy Perez <percyp3@gmail.com>
2018-07-18 11:29:04 -0700

    Test content_to_csv with mock

--- meetups/apps/tests/webscrape/test_webscrape.py
+++ meetups/apps/tests/webscrape/test_webscrape.py
@@ -1,4 +1,9 @@
-from apps.webscrape import get_content
+from mock import patch
+
+from apps.webscrape import content_to_csv, get_content
+
+
+target = 'apps.webscrape.get_content'
 
 
 class TestWebscrape(object):
@@ -6,3 +11,23 @@ class TestWebscrape(object):
         data = get_content()
 
         assert len(data) > 0
+
+    def test_mock_get_content(self, capsys):
+        with patch(target) as mock_wscrape:
+            mock_wscrape.return_value = [
+                'https://godjango.com',
+                'https://www.anaconda.com',
+                'http://www.blog.pythonlibrary.org'
+            ]
+            content_to_csv()
+
+            assert mock_wscrape.call_count == 1
+
+            out, err = capsys.readouterr()
+
+            assert out == (
+                'Link\r\n'
+                'https://godjango.com\r\n'
+                'https://www.anaconda.com\r\n'
+                'http://www.blog.pythonlibrary.org\r\n\r\n'
+            )
--- meetups/apps/webscrape.py
+++ meetups/apps/webscrape.py
@@ -1,12 +1,24 @@
 import bs4
+import csv
 import requests
+import sys
 
 url = "http://planetpython.org"
 source = requests.get(url)
 
 soup = bs4.BeautifulSoup(source.content, 'lxml')
 
+
 def get_content():
     links = [link.get('href') for link in soup.find_all('a')]
 
     return links
+
+
+def content_to_csv():
+    writer = csv.writer(sys.stdout)
+    links = get_content()
+    writer.writerow(['Link'])
+    for link in links:
+        writer.writerow([link])
+    writer.writerow([])
